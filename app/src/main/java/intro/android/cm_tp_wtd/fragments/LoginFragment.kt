b88526f672c0c781df540bd1d2c4a441cf300d04
Válter <valter07082001@gmail.com>
2024-05-19 16:29:13 +0100

    fragments6

--- app/src/main/java/intro/android/cm_tp_wtd/fragments/LoginFragment.kt
+++ app/src/main/java/intro/android/cm_tp_wtd/fragments/LoginFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import intro.android.cm_tp_wtd.R
@@ -18,6 +19,11 @@ class LoginFragment: Fragment() {
         val backButton = view.findViewById<ImageView>(R.id.backButton)
         backButton.setOnClickListener{
             findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
+        }
+
+        val registerButton = view.findViewById<TextView>(R.id.createAccountTextButton)
+        registerButton.setOnClickListener{
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
 
         return view
--- app/src/main/java/intro/android/cm_tp_wtd/fragments/MainFragment.kt
+++ app/src/main/java/intro/android/cm_tp_wtd/fragments/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import intro.android.cm_tp_wtd.R
@@ -22,7 +23,7 @@ class MainFragment : Fragment() {
             findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
         }
 
-        val createAccountButton = view.findViewById<ImageView>(R.id.createAccountButton)
+        val createAccountButton = view.findViewById<TextView>(R.id.createAccountButton)
         createAccountButton.setOnClickListener{
             findNavController().navigate(R.id.action_mainFragment_to_registerFragment)
         }
